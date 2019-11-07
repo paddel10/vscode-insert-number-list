e43f6aa6eec899312d4a12dc058d7fd7a91700a5
patrick <paddel10@hotmail.com>
2019-11-07 20:50:24 +0100

    cleanup

--- src/insert-number-list.js
+++ src/insert-number-list.js
@@ -1,15 +1,12 @@
-const generateIntegerList = require('./generate-integer-list.js');
 
 const main = vscode => {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
         const selections = editor.selections;
         if (selections) {
-            const integerList = generateIntegerList(selections);
             editor.edit(builder => {
                 selections.forEach((selection, i) => {
-                    let line = selection.start.line;
-                    builder.insert(selection.start, integerList[line].toString());
+                    builder.insert(selection.active, (i + 1).toString());
                 });
               });
         } else {
